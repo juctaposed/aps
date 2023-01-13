d4d3048bb5c43237f101a9ab9bbaab3a058bbb40
Justin <juc81@pitt.edu>
2023-01-13 14:22:20 -0500

    minor property changes

--- routes/property.js
+++ routes/property.js
@@ -5,8 +5,13 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Simple router for now, no liking properties just get viewing info working
 router.post("/search", propertyController.searchProperty);
-router.get("/search", propertyController.getBuildingInfo)
+// router.get("/search", propertyController.getBuildingInfo)
 
+// 
+//router.post("/search/bldg", controller.getBuildingInfo)
+//router.post("/search/tax", controller.getTaxInfo)
+//router.post("/search/owner", controller.getOwnerInfo)
+//router.post("/search/comp", controller.getComparableProperties)
 
 //Can just display through modal as opposed to pagination
 // router.get("/building", propertyController.getBuilding)
