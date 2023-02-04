836010ef9f672c59e2bfa6fca75ac18cd38f0ee2
Justin <juc81@pitt.edu>
2023-02-03 21:41:54 -0500

    removed post route, changed profile read to home controller

--- routes/main.js
+++ routes/main.js
@@ -3,14 +3,13 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
-const postsController = require("../controllers/posts");
 const contactController = require("../controllers/contact");
 const aboutController = require("../controllers/about")
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
 router.get("/", homeController.getIndex);
-router.get("/profile", ensureAuth, postsController.getProfile); 
+router.get("/profile", ensureAuth, homeController.getProfile); 
 // router.get("/recent", ensureAuth, postsController.getRecent);
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
