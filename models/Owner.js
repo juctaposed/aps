16e6fde14f04ef5bcb373227cce4c714a6213e4e
Justin <juc81@pitt.edu>
2023-03-22 23:55:50 -0400

    restructuring for readability

--- models/Owner.js
+++ models/Owner.js
@@ -1,18 +1,9 @@
 const mongoose = require('mongoose');
 
 const ownerHistorySchema = new mongoose.Schema({
-  owner: {
-    type: String,
-    required: true
-  },
-  saleDate: {
-    type: Date,
-    required: true
-  },
-  salePrice: {
-    type: Number,
-    required: true
-  }
+  owner: { type: String, required: true },
+  saleDate: { type: Date, required: true },
+  salePrice: { type: Number, required: true }
 });
 
 const OwnerModel = new mongoose.Schema({
--- models/Property.js
+++ models/Property.js
@@ -2,111 +2,44 @@ const mongoose = require("mongoose");
 
 const MarketValueModel = new mongoose.Schema({
   this_year: {
-    landValue: {
-      type: Number
-    },
-    buildingValue: {
-      type: Number
-    },
-    totalValue: {
-      type: Number
-    },
+    landValue: { type: Number }, 
+    buildingValue: { type: Number }, 
+    totalValue: { type: Number },
   },
   last_year: {
-    landValue: {
-      type: Number
-    },
-    buildingValue: {
-      type: Number
-    },
-    totalValue: {
-      type: Number
-    },
+    landValue: { type: Number },
+    buildingValue: { type: Number },
+    totalValue: { type: Number },
   }
 })
 
 const AssessedValueModel = new mongoose.Schema({
   this_year: {
-    landValue: {
-      type: Number
-    },
-    buildingValue: {
-      type: Number
-    },
-    totalValue: {
-      type: Number
-    },
+    landValue: { type: Number },
+    buildingValue: { type: Number },
+    totalValue: { type: Number },
   },
   last_year: {
-    landValue: {
-      type: Number
-    },
-    buildingValue: {
-      type: Number
-    },
-    totalValue: {
-      type: Number
-    },
+    landValue: { type: Number },
+    buildingValue: { type: Number },
+    totalValue: { type: Number },
   }
-})
+})  
 
 const PropertyModel = new mongoose.Schema({
-  parcelId: {
-    type: String,
-    require: true,
-  },
-
-  municipality: {
-    type: String,
-    required: true,
-  },
-  
-  address: {
-    type: String,
-    require: true,
-  },
-  
-  ownerName: {
-    type: String,
-    required: true,
-  },
-
-  ownerCode: {
-    type: String,
-    required: false
-  },
-
-  school: {
-    type: String,
-    required: true,
-  },
-  recordingDate: {
-    type: String,
-    required: false,
-  },
-
-  lotArea: {
-    type: Number,
-    required: false,
-  },
-  salePrice: {
-    type: Number,
-    required: false,
-  },
-
+  parcelId: { type: String, require: true, },
+  municipality: { type: String, required: true, },
+  address: { type: String, require: true, },
+  ownerName: { type: String, required: true, },
+  ownerCode: { type: String, required: false },
+  school: { type: String, required: true, },
+  recordingDate: { type: String, required: false, },
+  lotArea: { type: Number, required: false, },
+  salePrice: { type: Number, required: false, },
   fullMarketValues: MarketValueModel,
   countyAssessedValues: AssessedValueModel,
-
-  searchedBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-
-  dateSearched: {
-    type: Date,
-    default: Date.now,
-  },
-
+  searchedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", },
+  dateSearched: { type: Date, default: Date.now, },
 });
 
   
