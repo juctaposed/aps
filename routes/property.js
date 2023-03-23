0998e244eb5418c9dcca6b23ed15ff4bc15bb8bd
Justin <juc81@pitt.edu>
2023-03-22 23:56:35 -0400

    only /search propertyController

--- routes/property.js
+++ routes/property.js
@@ -3,23 +3,8 @@ const router = express.Router();
 const propertyController = require("../controllers/property");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
-//Simple router for now, no liking properties just get viewing info working
 router.post("/search", propertyController.searchProperty);
-//FOR CREATING ENDPOINTS IN PROPERTY PAGE MODALS
-// router.get("/search/countyTaxRecord/:parcelId", propertyController.getCountyTaxRecord);
-// router.get("/search/buildingInfo/:parcelId", propertyController.getBuildingInfo);
 
-
-// router.get("/search", propertyController.getBuildingInfo)
-
-// 
-//router.post("/search/bldg", controller.getBuildingInfo)
-//router.post("/search/tax", controller.getTaxInfo)
-//router.post("/search/owner", controller.getOwnerInfo)
-//router.post("/search/comp", controller.getComparableProperties)
-
-//Can just display through modal as opposed to pagination
-// router.get("/building", propertyController.getBuilding)
 
 
 module.exports = router;
